d6832327ac8baad57e651b22c62d19baa3150ff7
Enes <1eneskaraman@gmail.com>
2022-07-04 21:49:10 +0300

    ikinci bolum yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,8 +1,8 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println("Merhaba Dunya");
-
+        System.out.println("Merhaba Dunya2");
+        System.out.println("Merhaba Dunya3");
 
     }
 }
